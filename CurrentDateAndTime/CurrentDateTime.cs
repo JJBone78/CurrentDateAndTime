27c3ef2a891f0fb02b8c21be0e12a4bccdda9405
Jason Atkinson <jason.c.atkinson@hotmail.com>
2019-02-26 03:38:20 -0700

    Add project files.

---
+++ CurrentDateAndTime/CurrentDateTime.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrentDateAndTime
+{
+    class CurrentDateTime
+    {
+        static void Main(string[] args)
+        {
+            // Chapter 1, excercise 8
+            // Found the answer on StackOverlow.com, does solve the excercise
+            // Will return to polish and play with it and enhance it
+
+
+
+            DateTime now = DateTime.Now;
+            Console.WriteLine(now);
+                Console.ReadLine();
+
+
+          //  Console.WriteLine("Today's ")
+
+        }
+    }
+}
